eee5029cee001151f885d3e4a1d64c5fcf719a16
Abhinash Pradhan <abhinashpradhan8658@gmail.com>
2026-02-20 21:02:14 +0530

    Ready for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
-
 const app = express();
 
-const PORT = process.env.PORT || 3000; 
+// ✅correct way to create __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
